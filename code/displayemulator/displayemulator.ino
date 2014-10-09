2dd8dbd8ecca5a9858974b4ea113d10c52d1578a
Martin Gross <martin@pc-coholic.de>
2014-10-09 01:38:31 -0400

    Adding untested version of display-emulator

---
+++ code/displayemulator/displayemulator.ino
@@ -0,0 +1,61 @@
+// Nortel Millennium Payphone display-emulator
+// rudimentary emulator for a CU20026SCPB-T23A display
+//
+// Full pinout can be found at
+// http://wiki.muc.ccc.de/millennium:boards#connector_pinout
+//
+// This code aims at Arduino Nano w/ ATMega328.
+// If needed, change the pinout in the definitions below.
+//
+// This code comes with no warranty, blablabla.
+//
+// Please report bugfixes to https://github.com/muccc/millennium
+// or mail the author at martin@muc.ccc.de
+
+//     Arduino name / Arduino pin / Pin on VFD
+#define     d0             4    //Pin 15 on VFD
+#define     d1             5     //Pin 13 on VFD
+#define     d2             6     //Pin 11 on VFD
+#define     d3             7     //Pin 9 on VFD
+#define     d4             8     //Pin 7 on VFD
+#define     d5             9     //Pin 5 on VFD
+#define     d6            10     //Pin 3 on VFD
+#define     d7            11     //Pin 1 on VFD
+#define     x1            12     //Pin 17 on VFD
+/*
+#define     x2            NC     //Pin 19 on VFD
+#define     x3            NC     //Pin 21 on VFD
+#define     x4            NC     //Pin 23 on VFD
+#define     x5            NC     //Pin 25 on VFD
+#define     x6            NC     //Pin 20 on VFD
+*/
+
+void setup() {
+  pinMode(d0, INPUT);
+  pinMode(d1, INPUT);
+  pinMode(d2, INPUT);
+  pinMode(d3, INPUT);
+  pinMode(d4, INPUT);
+  pinMode(d5, INPUT);
+  pinMode(d6, INPUT);
+  pinMode(d7, INPUT);
+  pinMode(x1, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(x1) == 1) {
+    byte v;
+    bitWrite(v, 7, digitalRead(d0));
+    bitWrite(v, 6, digitalRead(d1));
+    bitWrite(v, 5, digitalRead(d2));
+    bitWrite(v, 4, digitalRead(d3));
+    bitWrite(v, 3, digitalRead(d4));
+    bitWrite(v, 2, digitalRead(d5));
+    bitWrite(v, 1, digitalRead(d6));
+    bitWrite(v, 0, digitalRead(d7));
+   
+   Serial.write(v);
+   //delay(150);
+  }
+}

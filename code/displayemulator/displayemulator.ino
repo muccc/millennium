71eb5071fbb95533b76a66312a2df4b112ba0e6b
Martin Gross <martin@pc-coholic.de>
2015-01-30 14:38:20 -0500

    new, non-working version of display-emulator

--- code/displayemulator/displayemulator.ino
+++ code/displayemulator/displayemulator.ino
@@ -13,16 +13,16 @@
 // or mail the author at martin@muc.ccc.de
 
 //     Arduino name / Arduino pin / Pin on VFD
-#define     d0             5     //Pin 15 on VFD
-#define     d1             6     //Pin 13 on VFD
-#define     d2             7     //Pin 11 on VFD
-#define     d3             8     //Pin 9 on VFD
-#define     d4             9     //Pin 7 on VFD
-#define     d5            10     //Pin 5 on VFD
-#define     d6            11     //Pin 3 on VFD
-#define     d7            12     //Pin 1 on VFD
-#define     x1             4     //Pin 17 on VFD
+#define     d0             3     //Pin 15 on VFD
+#define     d1             4     //Pin 13 on VFD
+#define     d2             5     //Pin 11 on VFD
+#define     d3             6     //Pin 9 on VFD
+#define     d4             7     //Pin 7 on VFD
+#define     d5             8     //Pin 5 on VFD
+#define     d6             9     //Pin 3 on VFD
+#define     d7            10     //Pin 1 on VFD
 /*
+#define     x1             4     //Pin 17 on VFD
 #define     x2            NC     //Pin 19 on VFD
 #define     x3            NC     //Pin 21 on VFD
 #define     x4            NC     //Pin 23 on VFD
@@ -39,33 +39,23 @@ void setup() {
   pinMode(d5, INPUT);
   pinMode(d6, INPUT);
   pinMode(d7, INPUT);
-  pinMode(x1, INPUT);
+  attachInterrupt(0, readInput, FALLING);
   Serial.begin(9600);
   Serial.write("Setup complete");
 }
 
 void loop() {
-  Serial.write(d0);
-  Serial.write(d1);
-  Serial.write(d2);
-  Serial.write(d3);
-  Serial.write(d4);
-  Serial.write(d5);
-  Serial.write(d6);
-  Serial.write(d7);
-  Serial.write(x1);
-  if (digitalRead(x1) == 1) {
-    byte v;
-    bitWrite(v, 7, digitalRead(d0));
-    bitWrite(v, 6, digitalRead(d1));
-    bitWrite(v, 5, digitalRead(d2));
-    bitWrite(v, 4, digitalRead(d3));
-    bitWrite(v, 3, digitalRead(d4));
-    bitWrite(v, 2, digitalRead(d5));
-    bitWrite(v, 1, digitalRead(d6));
-    bitWrite(v, 0, digitalRead(d7));
-   
-   Serial.write(v);
-   //delay(150);
-  }
+}
+
+void readInput() {
+  byte v;
+  bitWrite(v, 7, digitalRead(d0));
+  bitWrite(v, 6, digitalRead(d1));
+  bitWrite(v, 5, digitalRead(d2));
+  bitWrite(v, 4, digitalRead(d3));
+  bitWrite(v, 3, digitalRead(d4));
+  bitWrite(v, 2, digitalRead(d5));
+  bitWrite(v, 1, digitalRead(d6));
+  bitWrite(v, 0, digitalRead(d7));
+  Serial.write(v);
 }
